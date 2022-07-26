28fefa3d44870da8b08d681f9946b252f578d050
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-25 22:29:51 -0500

    module 2 completed

--- src/app.js
+++ src/app.js
@@ -13,9 +13,49 @@ app.set("view engine", "ejs");
 // Configure the Static Directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Read Account Data
+const accountData = fs.readFileSync(`${__dirname}/json/accounts.json`, "utf8");
+const accounts = JSON.parse(accountData);
+
+// Read User Data
+const userData = fs.readFileSync(`${__dirname}/json/users.json`, "utf8");
+const users = JSON.parse(userData);
+
 // Create the Index Route
+// Update the Index Route
 app.get("/", (req, res) => {
-  res.render("index", { title: "Index" });
+  res.render("index", {
+    title: "Account Summary",
+    accounts: accounts
+  });
+});
+
+// Create the Savings Account Route
+app.get("/savings", (req, res) => {
+  res.render("account", {
+    account: accounts.savings
+  });
+});
+
+// Create the Checking Account Route
+app.get("/checking", (req, res) => {
+  res.render("account", {
+    account: accounts.checking
+  });
+});
+
+// Create the Credit Account Route
+app.get("/credit", (req, res) => {
+  res.render("account", {
+    account: accounts.credit
+  });
+});
+
+// Create the Profile Route
+app.get("/profile", (req, res) => {
+  res.render("profile", {
+    user: users[0]
+  });
 });
 
 // Create a Server
